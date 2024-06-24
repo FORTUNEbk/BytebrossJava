8828e2104a933dc333d805a8fce526fed4d103a6
Fortunebk <kanyangif@gmail.com>
2024-06-24 21:17:10 +0300

    Printing a triangle

--- numberpyramid.java
+++ numberpyramid.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class numberpyramid {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the desired height");
+        System.out.println("Enter the desired height.");
         int number = Integer.valueOf(scanner.nextLine());
         
         for (int i = 1; i <= number; i++) {
